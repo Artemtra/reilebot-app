10641fa1137b72e0b7501f7a5dc0bf429993be50
MoOnStar <MoOnStar.artemreile@gmail.com>
2026-06-15 01:22:46 +1000

    Delete start.bat

--- start.bat
+++
@@ -1,4 +0,0 @@
-cd /d "C:\Users\artyo\OneDrive\Documents\_podgotovka\PracticTgBot"
-start "REILEBOT BOT" cmd /k "title BOT && python -m app.main"
-timeout /t 3 /nobreak >nul
-start "REILEBOT NGROK API" cmd /k "title NGROK API && ngrok http 8000"
